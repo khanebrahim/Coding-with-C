f6ab6105c8703e175c341d94e496a7e579f0363d
BengalDroid <bengaldroid@gmail.com>
2021-07-05 10:52:31 +0600

    comment

--- upper_case and lower_case/lower_case.c
+++ upper_case and lower_case/lower_case.c
@@ -3,6 +3,6 @@ int main()
 {
     char ch='A';
     char ch_small= ch+32;
-    printf("character:%c\n",ch_small);
+    printf("character:%c\n",ch_small); // upper_case to lower_case//
     return 0;
 }
