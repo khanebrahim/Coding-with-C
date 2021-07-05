f5d8c271012d0f691b16656da8d8d4b14b615e44
BengalDroid <bengaldroid@gmail.com>
2021-07-05 18:10:34 +0600

    leap year

---
+++ upper_case and lower_case/Leep-year.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(void)
+{
+int year =2021;
+int isLeapyear =0;
+if(year %4 =0){
+
+    if(year %100=0){
+
+        if(year %400 =0){
+
+            isLeapyear=1;
+        }else{
+            isLeapyear=0;
+        }
+    
+    
+    }else{
+        isLeapyear=1;
+    }
+
+    } else{
+        
+        isLeapyear = 0;
+    
+    }
+    if(isLeapyear){
+        printf("%d is a leapyear\n",year);
+    }
+else{
+    printf("%d is not a leap year\n",year);
+}
+
+return 0;
+
+}
+
+
