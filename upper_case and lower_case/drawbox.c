3d5100e9a67423da736db72735eb7b668de67f89
BengalDroid <bengaldroid@gmail.com>
2021-07-09 11:46:16 +0600

    drawbox

---
+++ upper_case and lower_case/drawbox.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int width =10, height=5,i,j;
+    for(i=0; i<height;i++){
+    for(j=0;j<width;j++){
+        printf("*");
+    }
+    
+    printf("\n");
+    }
+    
+    return 0;
+}
