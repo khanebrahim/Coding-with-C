656e7dc2199e05da6410cc84dd26624e1182247d
BengalDroid <bengaldroid@gmail.com>
2021-07-10 12:46:15 +0600

    change lucky number

--- upper_case and lower_case/guessing.c
+++ upper_case and lower_case/guessing.c
@@ -6,15 +6,21 @@ int main(void)
 {
     time_t t;
     srand((unsigned)time(&t));
-    int choosen_number;
+    int choosen_number ,chance=3;
+    bool is_loss =true;
+
     int lucky_number = rand() %10+1;
-    while(true){
+    while(chance>0){
         printf("Guess the lucky number:");
         scanf("%d", &choosen_number);
         if (lucky_number == choosen_number){
             printf("congratulation! you have choose the correct number\n");
             break;
         }
+        chance --;
+    }
+    if(is_loss){
+        printf("you have loss. lucky number is %d\n", lucky_number);
     }
 
     return 0;
