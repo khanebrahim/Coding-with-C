90c2519313bb2bab842d31dc5e86c871c47b46d5
BengalDroid <bengaldroid@gmail.com>
2021-07-12 14:15:11 +0600

    full pyramid

--- upper_case and lower_case/star.c
+++ upper_case and lower_case/star.c
@@ -1,11 +1,25 @@
 #include<stdio.h>
 int main(){
     int i,j,rows=5;
-    for (i=0;i<rows;i++){
-        for (j=0;j<i;j++){
-            printf("* ");
-        }
-        printf("\n");
+    // half pyramid
+     for (i=0;i<rows;i++){
+         for (j=0;j<i;j++){
+             printf("* ");
+         }
+         printf("\n");
     }
+
+//    full pyramid 
+for (i=0;i<rows;i++){
+for (j=i+1;j<rows;j++){
+    printf(" ");
+}
+for(j=0;j<(2*i)-1;j++){
+    printf("*");
+}
+printf("\n");
+}
+
+
     return 0;
 }
